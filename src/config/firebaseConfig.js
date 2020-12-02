24ccccd5e9da9710983de3070ea4a5e7874b8417
Manish Rajak <manish.rajak2055@gmail.com>
2020-12-02 19:28:11 +0545

    second commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBa7-jsXWStZGXXJ_OSzbWjuC8j_tg58T0",
+    authDomain: "techsansaar-bd473.firebaseapp.com",
+    databaseURL: "https://techsansaar-bd473.firebaseio.com",
+    projectId: "techsansaar-bd473",
+    storageBucket: "techsansaar-bd473.appspot.com",
+    messagingSenderId: "701137678396",
+    appId: "1:701137678396:web:7ee32dd75a31ce4f98d339",
+    measurementId: "G-D64DHJ0Z0M"
+  };
+
+  export default config;
