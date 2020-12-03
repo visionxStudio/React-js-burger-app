a66d449af01a9c835f4ddbb1a9f3d821c3159a72
Manish Kumar Rajak <52307777+visionxStudio@users.noreply.github.com>
2020-12-03 11:57:37 +0545

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 
 const config = {
-    apiKey: "AIzaSyBa7-jsXWStZGXXJ_OSzbWjuC8j_tg58T0",
-    authDomain: "techsansaar-bd473.firebaseapp.com",
-    databaseURL: "https://techsansaar-bd473.firebaseio.com",
-    projectId: "techsansaar-bd473",
-    storageBucket: "techsansaar-bd473.appspot.com",
-    messagingSenderId: "701137678396",
-    appId: "1:701137678396:web:7ee32dd75a31ce4f98d339",
-    measurementId: "G-D64DHJ0Z0M"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   const fire = firebase.initializeApp(config);
